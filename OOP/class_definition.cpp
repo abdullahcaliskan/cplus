9fc11ad867f886a8525d96d905edf9570d53061d
Abdullah <abdullahcaliskan511@gmail.com>
2016-11-29 12:14:46 +0300

    Cplus nesne yapısı görmek

---
+++ OOP/class_definition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Box {
+	public:
+		double uzunluk;
+		double derinlik;
+		double yukseklik;
+
+	
+}
+
+
+int main()
+{
+	Box kutu1; // Kutu nesnesini oluşturduk
+
+	kutu1.uzunluk = 10.0;
+	kutu1.genislik = 12.0;
+	kutu1.derinlik = 5.0;
+
+	Box kutu2;
+
+	kutu2.uzunluk = 20.0;
+	kutu2.genislik = 25.0;
+	kutu2.derinlik = 12.0;
+
+}
