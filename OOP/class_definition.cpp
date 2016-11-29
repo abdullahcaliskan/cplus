21c842432642194c7a89333a61d80fff1f3e7688
Abdullah <abdullahcaliskan511@gmail.com>
2016-11-29 12:26:00 +0300

    class methodlarının kullanılması

--- OOP/class_definition.cpp
+++ OOP/class_definition.cpp
@@ -8,8 +8,12 @@ class Box {
 		double derinlik;
 		double yukseklik;
 
-	
-}
+		// function inline definition
+		double hacimHesapla(void)
+		{
+			return (uzunluk * derinlik * yukseklik);
+		}
+};
 
 
 int main()
@@ -17,13 +21,15 @@ int main()
 	Box kutu1; // Kutu nesnesini oluşturduk
 
 	kutu1.uzunluk = 10.0;
-	kutu1.genislik = 12.0;
+	kutu1.yukseklik = 12.0;
 	kutu1.derinlik = 5.0;
 
 	Box kutu2;
 
 	kutu2.uzunluk = 20.0;
-	kutu2.genislik = 25.0;
+	kutu2.yukseklik = 25.0;
 	kutu2.derinlik = 12.0;
 
+	// Methodların kullanılması
+	cout << kutu1.hacimHesapla() << endl;
 }
